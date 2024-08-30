742ff2acd0fba2c203c5326d68cc11041673e7ec
defsanmith <asanmith@gmail.com>
2024-08-30 12:26:24 +0530

    1. **Prisma Schema (`schema.prisma`)**:     - Added models for `User`, `Account`, `Session`, `VerificationToken`, and `Authenticator`.
    2. **NextAuth Configuration (`route.ts`)**:
        - Added session management with JWT strategy.
        - Configured Prisma as a database provider.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,23 @@
 import { GITHUB_ID, GITHUB_SECRET } from "@/constants/environment";
+import prisma from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import { Adapter } from "next-auth/adapters";
 import GitHubProvider from "next-auth/providers/github";
 
+const prismaAdapter = PrismaAdapter(prisma) as Adapter;
+
 const handler = NextAuth({
+  adapter: prismaAdapter,
   providers: [
     GitHubProvider({
       clientId: GITHUB_ID,
       clientSecret: GITHUB_SECRET,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 });
 
 export { handler as GET, handler as POST };
